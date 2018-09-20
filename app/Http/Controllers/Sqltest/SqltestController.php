b0600ad6fceab953ae97df93f985cc56894b328f
yuepeng <yuepeng1@100tal.com>
2018-09-20 15:24:52 +0800

    新增sqltest

---
+++ app/Http/Controllers/Sqltest/SqltestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Sqltest;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SqltestController extends Controller
+{
+    public function index(Request $request)
+    {
+
+
+
+    }
+}
